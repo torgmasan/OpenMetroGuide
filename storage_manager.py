5be379859cca5de8806ca2f239d6e962b0079449
yde773786 <yde773786@gmail.com>
2021-04-09 17:06:18 +0400

    Created storage_manager.py

---
+++ storage_manager.py
@@ -0,0 +1,5 @@
+"""This file is the manager of the links to the database of
+OpenMetroGuide (currently local database). It will handle reading and writing
+to be used by all pygame windows that interact with stored Metro lines.
+"""
+import sqlite3
