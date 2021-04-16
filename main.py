e85434a09637b697d1941c514a41dca79dbd8203
sidsachdev12 <sid.sachdev@mail.utoronto.ca>
2021-04-17 00:24:24 +0400

    cleaned main.py a bit.

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def run_home() -> None:
     init_db()
 
     pygame.display.set_caption("OpenMetroGuide")
-    current_user = 'admin'
+    current_user = 'admin'  # admin or client
     chk = True
-    screen_type = 0  # screen type can be 0/1 if main screen or instruction set.
+    screen_type = 0  # screen type can be 0/1/2 if main screen or instruction set.
 
-    queue_lst = get_cities()
-    current_index = 0
-    current_opt = 0
+    queue_lst = get_cities()    # Contains all cities
+    current_index = 0   # index for queue_lst
+    current_opt = 'use'  # 'use' a pre-existing map or 'create' a new map
     enter_city_rect = pygame.Rect((110, 150, 200, 30))
     warning = ''
     city_name = ''
@@ -35,57 +35,61 @@ def run_home() -> None:
 
         screen.fill(WHITE)
 
-        if screen_type == 0:
-            refresh_display(screen, screen_type)
-        elif screen_type == 1:
-            refresh_display(screen, screen_type, queue_lst, current_user, current_index, current_opt)
-        else:
-            refresh_display(screen, screen_type, active_color=THECOLORS['blue'],
-                            name_rect=enter_city_rect)
+        _display_correct_screen(screen_type=screen_type, screen=screen, queue_lst=queue_lst,
+                                current_user=current_user, current_opt=current_opt,
+                                current_index=current_index, enter_city_rect=enter_city_rect)
 
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 sys.exit()
+
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    if screen_type == 0:
-                        current_user = 'client'
-                    elif current_user == 'admin' and queue_lst:
-                        # Key down only used by admin with multiple choices
-                        current_opt = 1
-                elif event.key == pygame.K_UP:
-                    if screen_type == 0:
-                        current_user = 'admin'
-                    else:
-                        current_opt = 0
+                current_user, current_opt = _handle_up_down_key(event=event,
+                                                                screen_type=screen_type,
+                                                                current_user=current_user,
+                                                                queue_lst=queue_lst,
+                                                                current_opt=current_opt)
+
                 if event.key == pygame.K_RIGHT and screen_type == 1:
                     current_index = (current_index + 1) % len(queue_lst)
+
                 elif event.key == pygame.K_LEFT and screen_type == 1:
                     current_index = (current_index - 1) % len(queue_lst)
+
                 elif event.key == pygame.K_RETURN:
+
                     if screen_type == 1 and ((current_user == 'client') or
-                                             (queue_lst and current_opt == 0)):
+                                             (queue_lst and current_opt == 'use')):
                         # Client stops at screen_type 1
                         # Admin who chose to use existing map stops
                         # at type 1
                         chk = False
                         break
+
                     elif screen_type == 2 and current_user == 'admin':
                         # Admin creating new map stops at screen_type 2
+
                         if city_name == '':
                             warning = 'No input provided'
+
                         elif city_name in queue_lst:
                             warning = 'Name already present'
+
                         else:
                             chk = False
                             break
+
                     else:
                         screen_type += 1
+
                 else:
+
                     if screen_type == 2:
                         city_name = _handle_event_for_run_home(event, city_name)
 
         if chk and screen_type == 0:
+
             set_selection(screen, current_user)
 
         name_surface = base_font.render(city_name, True, (0, 0, 0))
@@ -95,16 +99,57 @@ def run_home() -> None:
         pygame.display.flip()
 
     metro_map = Map()
-    if current_opt == 0 and queue_lst:
+
+    if current_opt == 'use' and queue_lst:
         city_name = queue_lst[current_index]
         metro_map = get_map(city_name)
 
     if current_user == 'admin':
         admin = user.Admin(city_name, metro_map)
         admin.display()
+
     else:
         client = user.Client(metro_map, city_name)
         client.display()
+
+
+def _display_correct_screen(screen_type: int, screen: pygame.surface, queue_lst: list[str],
+                            current_user: str, current_index: int, current_opt: str,
+                            enter_city_rect: pygame.Rect) -> None:
+    """Helper function to ...
+    """
+    if screen_type == 0:
+        refresh_display(screen, screen_type)
+
+    elif screen_type == 1:
+        refresh_display(screen, screen_type, queue_lst, current_user, current_index, current_opt)
+
+    else:
+        refresh_display(screen, screen_type, active_color=THECOLORS['blue'],
+                        name_rect=enter_city_rect)
+
+
+def _handle_up_down_key(event: pygame.event, screen_type: int,
+                        current_user: str, queue_lst: list[str],
+                        current_opt: str) -> tuple[str, str]:
+    """Helper function to ..."""
+    if event.key == pygame.K_DOWN:
+
+        if screen_type == 0:
+            current_user = 'client'
+
+        elif current_user == 'admin' and queue_lst:
+            # Key down only used by admin with multiple choices
+            current_opt = 'create'
+
+    elif event.key == pygame.K_UP:
+
+        if screen_type == 0:
+            current_user = 'admin'
+        else:
+            current_opt = 'use'
+
+    return (current_user, current_opt)
 
 
 def _handle_event_for_run_home(event: pygame.event.Event, name: str) -> str:
@@ -119,7 +164,7 @@ def _handle_event_for_run_home(event: pygame.event.Event, name: str) -> str:
 
 
 def refresh_display(screen: pygame.Surface, screen_type: int, queue_lst: list = None,
-                    current_user: str = None, current_index: int = None, current_opt: int = None,
+                    current_user: str = None, current_index: int = None, current_opt: str = None,
                     active_color: tuple[int, int, int] = None,
                     name_rect: pygame.Rect = None) -> None:
     """Part of the GUI that remains constant throughout
@@ -136,7 +181,9 @@ def refresh_display(screen: pygame.Surface, screen_type: int, queue_lst: list = 
                          (5, 100, screen.get_width() - 10, 55), 10)
         pygame.draw.rect(screen, BLACK,
                          (5, 200, screen.get_width() - 10, 55), 10)
+
     elif screen_type == 1:  # Choosing Station or creating new map window
+
         if queue_lst and current_user == 'client':
             draw_text(screen, 'Select City (Right/Left)', 25,
                       (120, 50), BLACK)
@@ -152,22 +199,27 @@ def refresh_display(screen: pygame.Surface, screen_type: int, queue_lst: list = 
                       (20, 50), BLACK)
             draw_text(screen, '->', 40, (350, 135), BLACK)
             draw_text(screen, '<-', 40, (50, 135), BLACK)
-            if current_opt == 0:
+
+            if current_opt == 'use':
                 font = pygame.font.Font(None, 25)
                 text = font.render(queue_lst[current_index], True, THECOLORS['green'])
                 text_rect = text.get_rect(center=(200, 150))
                 screen.blit(text, text_rect)
                 draw_text(screen, '+ Create New Map', 35, (100, 250), BLACK)
+
             else:
                 font = pygame.font.Font(None, 25)
                 text = font.render(queue_lst[current_index], True, BLACK)
                 text_rect = text.get_rect(center=(200, 150))
                 screen.blit(text, text_rect)
                 draw_text(screen, '+ Create New Map', 35, (100, 250), THECOLORS['green'])
+
         elif not queue_lst and current_user == 'client':
             draw_text(screen, 'No map in Database', 35, (90, 120), BLACK)
+
         else:
             draw_text(screen, '+ Create New Map', 35, (90, 120), THECOLORS['green'])
+
     else:  # Entering the city name
         draw_text(screen, 'Enter City Name', 30,
                   (120, 50), BLACK)
@@ -186,6 +238,7 @@ def set_selection(screen: pygame.Surface, selected: str):
                          (5, 100, screen.get_width() - 10, 55), 3)
         pygame.draw.rect(screen, WHITE,
                          (5, 200, screen.get_width() - 10, 55), 3)
+
     else:
         pygame.draw.rect(screen, WHITE,
                          (5, 100, screen.get_width() - 10, 55), 3)
