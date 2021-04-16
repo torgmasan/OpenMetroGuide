4063a18e0c7c7f2b85748da4e1f132920f4f3e0c
imanshjain <imanshjain@gmail.com>
2021-04-17 02:27:51 +0400

    waiting for sid to say 'all done fully complete'

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 """This is the module from which the program is to be run.
 
 INSTRUCTIONS:
-    The project_report.pdf contains a subsection of the Instructions
-    that provide a detailed understanding, with pictures of a
-    successful working, of how to go about using the application.
+    The project_report.pdf contains a subsection titled 'Instructions'
+    that provides a detailed understanding, with pictures, of a
+    successful working and how to go about using the application.
 
 NOTES:
     - The arrows in the second window (if cities are already present in the dataset)
-       are for visual aid and not clickable. ALL NAVIGATION IN HOME IS THROUGH ARROW
-       KEYS.
+      are for visual aid and not clickable. ALL NAVIGATION IN HOME IS THROUGH ARROW
+      KEYS.
     - To proceed to the next step/window, pressing the enter/return key will suffice.
       The exceptions to this is the final step of creating/editing the map. After making
       the changes required, CLOSE the window. The data is auto-saved in the database.
